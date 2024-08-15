f1f72b276dca8b574a932b86ab417d0778b6b048
Bhumitpatel1492 <patelbhumit484@gmail.com>
2024-08-15 19:37:35 +0530

    [ADD] Register Scrren Setup done

--- App.js
+++ App.js
@@ -2,7 +2,9 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './Src/Screens/LoginScreen';
+import Login from './Src/Screens/LoginScreen';
+import Register from './Src/Screens/RegisterScreen';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -12,8 +14,13 @@ const App = () => {
       <Stack.Navigator initialRouteName="LoginScreen">
         <Stack.Screen
           options={{ headerShadowVisible: false }}
-          name="LoginScreen"
-          component={LoginScreen}
+          name="Login"
+          component={Login}
+        />
+        <Stack.Screen
+          options={{ headerShadowVisible: false }}
+          name="Register"
+          component={Register}
         />
       </Stack.Navigator>
     </NavigationContainer>
--- Src/Screens/LoginScreen/index.js
+++ Src/Screens/LoginScreen/index.js
@@ -1,11 +1,12 @@
 //import libraries
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Image, KeyboardAvoidingView } from 'react-native';
 import auth from '@react-native-firebase/auth';
 import CustomToastMessage from '../../Components/CustomToastMsg';
 
+
 // create a component
-const LoginScreen = () => {
+const Login = ({ navigation }) => {
   const [Email, SetEmail] = useState('');
   const [password, SetPassword] = useState('');
   const [isVisible, setVisible] = useState(false);
@@ -53,33 +54,52 @@ const LoginScreen = () => {
     }, 3000); // Hide the toast after 3 seconds
   };
 
+  const registeruser = () => {
+    navigation.navigate('Register')
+  }
+
   return (
     <View style={styles.container}>
-      <TextInput
-        value={Email}
-        onChangeText={txt => SetEmail(txt)}
-        placeholder='Enter Email'
-        style={styles.input}
-      />
-      <TextInput
-        value={password}
-        onChangeText={txt => SetPassword(txt)}
-        placeholder='Enter Password'
-        secureTextEntry={true}
-        style={styles.input}
-      />
-      <CustomToastMessage
-        visible={isVisible}
-        isSuccess={isSuccess}
-        message={message}
-        onClose={() => setVisible(false)}
-      />
-      <TouchableOpacity
-        style={styles.loginButton}
-        onPress={loginuser}
-      >
-        <Text style={styles.loginButtonText}>Login</Text>
-      </TouchableOpacity>
+      <KeyboardAvoidingView>
+        <View>
+          <Image
+            source={require('../../../Assets/Images/login1.png')}
+            resizeMode='contain'
+            style={{ height: 250, width: 340, justifyContent: "center" }}
+          />
+        </View>
+        <TextInput
+          value={Email}
+          onChangeText={txt => SetEmail(txt)}
+          placeholder='Enter Email'
+          style={styles.input}
+        />
+        <TextInput
+          value={password}
+          onChangeText={txt => SetPassword(txt)}
+          placeholder='Enter Password'
+          secureTextEntry={true}
+          style={styles.input}
+        />
+        <CustomToastMessage
+          visible={isVisible}
+          isSuccess={isSuccess}
+          message={message}
+          onClose={() => setVisible(false)}
+        />
+        <TouchableOpacity
+          style={styles.loginButton}
+          onPress={loginuser}
+        >
+          <Text style={styles.loginButtonText}>Login</Text>
+        </TouchableOpacity>
+
+        <View style={{ marginVertical: 18, alignItems: "flex-end" }}>
+          <TouchableOpacity onPress={() => registeruser()}>
+            <Text >{'Dont have account ? Resgister here '}</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
     </View>
   );
 };
@@ -88,8 +108,8 @@ const LoginScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     paddingHorizontal: 16,
+    backgroundColor: "#fff"
   },
   input: {
     padding: 10,
@@ -111,4 +131,4 @@ const styles = StyleSheet.create({
 });
 
 //make this component available to the app
-export default LoginScreen;
+export default Login;
